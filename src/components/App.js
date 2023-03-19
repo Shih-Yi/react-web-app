e8f273c8708275179387b526b6b059f4b20af963
Shih-Yi <brvast@gmail.com>
2023-03-20 11:05:14 +1300

    move App and Header under components folder

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "./ui/Header"
+
+function App() {
+  return (
+    <div className="APP">
+      <Header />
+      <div>
+        <h1>Hello World</h1>
+        <br />
+      </div>
+    </div>
+  )
+}
+
+export default App
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,24 +3,20 @@ import AppBar from "@mui/material/AppBar"
 import Toolbar from "@mui/material/Toolbar"
 import useScrollTrigger from "@mui/material/useScrollTrigger"
 
-function ElevationScroll(props) {
+const ElevationScroll = (props) => {
   const { children, window } = props
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
   const trigger = useScrollTrigger({
     disableHysteresis: true,
     threshold: 0,
     target: window ? window() : undefined,
   })
-
   return React.cloneElement(children, {
-    elevation: trigger ? 5 : 0,
+    elevation: trigger ? 4 : 0,
   })
 }
 
-const Header = () => (
-  <ElevationScroll>
+const Header = (props) => (
+  <ElevationScroll {...props}>
     <AppBar>
       <Toolbar>React MUI APP</Toolbar>
     </AppBar>
